c441fb0e3fd6d61763ef9869d1c87ed10df739a5
SelimHorri <horri.slim@gmail.com>
2020-10-31 17:12:39 +0100

    some front

--- src/main/java/com/selimhorri/app/pack/controllers/EmployeeController.java
+++ src/main/java/com/selimhorri/app/pack/controllers/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EmployeeController {
 	
 	@GetMapping(value = {"/"})
-	public String index() {
+	public String displayIndex() {
 		return "home";
 	}
 	
