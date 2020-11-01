877b1833643e3d2429591088fbdeaf9ebea1f151
SelimHorri <horri.slim@gmail.com>
2020-11-01 08:40:39 +0100

    employees-list&departments-list

--- src/main/java/com/selimhorri/app/pack/controllers/EmployeeController.java
+++ src/main/java/com/selimhorri/app/pack/controllers/EmployeeController.java
@@ -1,14 +1,17 @@
 package com.selimhorri.app.pack.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
 	
 	@GetMapping(value = {"/"})
-	public String displayIndex() {
-		return "home";
+	public String displayIndex(final Model model) {
+		
+		model.addAttribute("name", "selim");
+		return "home/home";
 	}
 	
 	
